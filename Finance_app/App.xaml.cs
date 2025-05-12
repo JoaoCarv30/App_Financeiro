5e3c42bbaa64f6ce0f800bbbfe5cb15d1b1d6930
JoaoCarv30 <Carvalhoestudante1@gmail.com>
2025-05-12 19:55:45 -0300

    initial commit

---
+++ Finance_app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Finance_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Finance_app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Finance_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
