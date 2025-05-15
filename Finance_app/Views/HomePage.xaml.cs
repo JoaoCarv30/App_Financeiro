fb8715b90b0fd11a2564731cb91ae5b31b8d5f07
JoaoCarv30 <Carvalhoestudante1@gmail.com>
2025-05-14 22:38:40 -0300

    HomePage initial design

--- Finance_app/Views/HomePage.xaml.cs
+++ Finance_app/Views/HomePage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Finance_app.ViewModels;
 
 namespace Finance_app.Views;
 
@@ -11,5 +12,11 @@ public partial class HomePage : ContentPage
     public HomePage()
     {
         InitializeComponent();
+        BindingContext = new HomePageViewModel();
+    }
+
+    private void OnDrinkClicked(object? sender, EventArgs e)
+    {
+        DisplayAlert("Icone", "Cliquei no ícone de bebida", "OK");
     }
 }
