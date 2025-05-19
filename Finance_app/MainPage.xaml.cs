acb87657d0d703b0e432ede1943d43b4b2f5e0e8
JoaoCarv30 <Carvalhoestudante1@gmail.com>
2025-05-19 19:47:42 -0300

    Merge code

--- Finance_app/MainPage.xaml.cs
+++ Finance_app/MainPage.xaml.cs
@@ -10,7 +10,37 @@ namespace Finance_app
         {
             InitializeComponent();
         }
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            string email = EntryEmail.Text;
+            string Password = EntryPassword.Text;
 
+            //Teste
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(Password))
+            {
+                ErrorMsg.Text = "Preencha todos os campos";
+                ErrorMsg.IsVisible = true;
+                return;
+
+            }
+
+            if (email == "juan@maui.com" && Password == "1234")
+            {
+                ErrorMsg.IsVisible = false;
+                await Navigation.PushAsync(new MainPage());
+
+            }
+
+            else
+            {
+                ErrorMsg.Text = "Suas credenciais são inválidas.";
+                ErrorMsg.IsVisible = true;
+
+            }
+        }
+        
+        
         private async void OnCounterClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync(nameof(HomePage));
